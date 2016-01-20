4a18502ae05c19ee4f8a7eaed42382485a050943
Michael Alves Pereira <leer@leer.com>
2016-01-20 08:30:14 +0100

    Miau

--- rewsfew/src/rewsfewd/name.java
+++ rewsfew/src/rewsfewd/name.java
@@ -2,9 +2,9 @@ package rewsfewd;
 
 public class name {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static void main(String[] args) 
+	{
+		System.out.println("Miau Miau");
 	}
 
 }
